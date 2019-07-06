c3ddee689754f5b7f1b353105eeda5b5f93ee28f
Yevgen Patiy on Sofia's PC <evgenpatiy@gmail.com>
2019-07-06 16:31:35 +0300

    add javafx

--- src/main/java/ua/com/foxminded/sanitizer/Main.java
+++ src/main/java/ua/com/foxminded/sanitizer/Main.java
@@ -4,23 +4,35 @@ import java.io.IOException;
 
 import com.github.difflib.algorithm.DiffException;
 
-import ua.com.foxminded.sanitizer.patch.PatchData;
-import ua.com.foxminded.sanitizer.worker.FileWorker;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import ua.com.foxminded.sanitizer.ui.StartWindow;
 
-public class Main {
+public class Main extends Application {
     public static void main(String[] args) throws DiffException, IOException {
-        String originalFileName = "/mnt/500GB/BACKUP/SANITIZER/ShapeGlobal.java";
-        String processedFileName = "/mnt/500GB/BACKUP/SANITIZER/ShapeGlobal_comments2.java";
-        String patchFileName = "/mnt/500GB/BACKUP/SANITIZER/patch.xml";
-
-        FileWorker fw = new FileWorker(originalFileName, processedFileName, patchFileName);
-        PatchData patchData;
-
-        if ((patchData = fw.getPatchDataFromDiff()) != null) {
-            // new XMLPatchWorker().writePatchData(new File(patchFileName), patchData);
-            fw.updatePatchData();
-        }
+        /*
+         * String originalFileName = "/mnt/500GB/BACKUP/SANITIZER/ShapeGlobal.java";
+         * String processedFileName =
+         * "/mnt/500GB/BACKUP/SANITIZER/ShapeGlobal_comments2.java"; String
+         * patchFileName = "/mnt/500GB/BACKUP/SANITIZER/patch.xml";
+         * 
+         * FileWorker fw = new FileWorker(originalFileName, processedFileName,
+         * patchFileName); PatchData patchData;
+         * 
+         * if ((patchData = fw.getPatchDataFromDiff()) != null) { // new
+         * XMLPatchWorker().writePatchData(new File(patchFileName), patchData);
+         * fw.updatePatchData(); }
+         */
         // System.out.println(new XMLPatchWorker().readPatchData(configFile,
         // PatchData.class));
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Application.setUserAgentStylesheet(Application.STYLESHEET_MODENA);
+        // StyleManager.getInstance().addUserAgentStylesheet("/css/checker.css");
+        // mainWindow.showMainWindow(primaryStage);
+        new StartWindow().show(primaryStage);
     }
 }
--- src/main/java/ua/com/foxminded/sanitizer/ui/StartWindow.java
+++ src/main/java/ua/com/foxminded/sanitizer/ui/StartWindow.java
@@ -1,5 +1,18 @@
 package ua.com.foxminded.sanitizer.ui;
 
-public class StartWindow {
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
+public class StartWindow {
+    public void show(Stage stage) {
+        BorderPane root = new BorderPane();
+        int mainW = 1024;
+        int mainH = 600;
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("/img/code.png")));
+        stage.setScene(new Scene(root, mainW, mainH));
+        stage.setTitle("title");
+        stage.show();
+    }
 }
