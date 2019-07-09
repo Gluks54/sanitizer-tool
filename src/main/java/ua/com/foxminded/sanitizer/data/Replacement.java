dfa21675104b3fd65b0b04af76894355cd291f79
Yevgen Patiy <evgenpatiy@gmail.com>
2019-07-09 20:25:52 +0300

    fix xml

--- src/main/java/ua/com/foxminded/sanitizer/data/Replacement.java
+++ src/main/java/ua/com/foxminded/sanitizer/data/Replacement.java
@@ -1,11 +1,16 @@
 package ua.com.foxminded.sanitizer.data;
 
+import javax.xml.bind.annotation.XmlRootElement;
+
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 @RequiredArgsConstructor
+@XmlRootElement(name = "repl")
 public class Replacement {
     @NonNull
     private String source;
